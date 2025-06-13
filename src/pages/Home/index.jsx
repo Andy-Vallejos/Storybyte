fd059aad51cdbbfee90190218ecee78511ad5564
Andy Vallejos <andyvallejosgb@gmail.com>
2025-06-13 16:44:26 -0400

    images: add images in sections

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,36 +1,37 @@
 import "./home.css"
 
-function Card({ name, color, children }) {
+function Card({ name, color, picture, children }) {
   return (
     <section className={`${name} category home__new__banner`} style={{ background: color, boxShadow: "0px 0px 20px 2px" + color }}>
       {children}
+      <img src={picture} alt="" />
     </section>
   )
 }
 
 export default function Home() {
   const categorias = [
-    { name: 'Terror', color: "#6B8E23" },
-    { name: 'Infantil', color: "#FFD700" },
-    { name: 'Accion', color: "#0D47A1" },
-    { name: 'Romance', color: "#FF69B4" },
-    { name: 'Fantasia', color: "#724DE0" },
-    { name: 'Misterio', color: "#2F4F4F" }
+    { name: 'Terror', color: "#6B8E23", picture: "/public/ghost.png" },
+    { name: 'Infantil', color: "#FFD700", picture: "/public/ballons.png" },
+    { name: 'Accion', color: "#0D47A1", picture: "/public/soldier.png" },
+    { name: 'Romance', color: "#FF69B4", picture: "/public/heart.png" },
+    { name: 'Fantasia', color: "#724DE0", picture: "/public/fairy-tale.png" },
+    { name: 'Misterio', color: "#2F4F4F", picture: "/public/moon.png" }
   ]
   return (
     <div className="home">
       < section className="home__new" >
         <h1>Nuevo</h1>
-        <Card color={"#64A5A6"}>
+        <Card color={"#64A5A6"} picture={"/public/books.png"}>
           <h1>Libros <br /> de la semana</h1>
         </Card>
       </section >
       <section className="home__categories">
         <h1>Categorias</h1>
         <section className="home__categories__wall">
-          {categorias.map((category) => {
-            return <Card name={category.name} color={category.color}>
-              <h1>{category.name}</h1>
+          {categorias.map((el) => {
+            return <Card name={el.name} color={el.color} picture={el.picture}>
+              <h1>{el.name}</h1>
             </Card>
           })}
         </section>
